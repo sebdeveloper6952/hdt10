8a18a6aec506ac4dd40de91e51df123b425d1748
Unknown <sebdeveloper6952@gmail.com>
2018-05-13 23:26:08 -0600

    Agregar neo4jdb.cs
    Clase que se encarga de la conección con neo4j, y mas adelante tendrá todos los querys.

---
+++ hdt10/hdt10/neo4jdb.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neo4j.Driver.V1;
+
+namespace hdt10
+{
+    public class neo4jdb : IDisposable
+    {
+        public static neo4jdb instance = new neo4jdb("bolt://localhost:7687", "neo4j", "pass123");
+        private readonly IDriver driver;
+
+        private neo4jdb(string uri, string user, string password)
+        {
+            driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+        }
+
+        public void Dispose()
+        {
+            driver?.Dispose();
+        }
+    }
+}
